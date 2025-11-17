8073df702047510d9a579d6640cb57c1b090ff6c
tvanlam <tzanlam@gmail.com>
2025-11-17 12:13:13 +0700

    api review

--- backend/src/main/java/bag/support/method/Support.java
+++ backend/src/main/java/bag/support/method/Support.java
@@ -5,6 +5,10 @@ import bag.modal.entity.OrderDetails;
 import bag.modal.entity.Voucher;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Support {
     public static String buildKey(String email, String action){
@@ -15,5 +19,19 @@ public class Support {
         return String.format("%,.0f đ", amount);
     }
 
+    public static LocalDate convertStringToLocalDate(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        return LocalDate.parse(date, formatter);
+    }
+
+    public static LocalTime convertStringToLocalTime(String time){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return LocalTime.parse(time, formatter);
+    }
+
+    public static LocalDateTime convertStringToLocalDateTime(String dateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        return LocalDateTime.parse(dateTime, formatter);
+    }
 
 }
